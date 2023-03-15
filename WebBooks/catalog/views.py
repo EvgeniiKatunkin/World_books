2b0b2c63cdbcd48540c7fc7e1c79abbe60a992ce
Evgenii <evgeniikatunkin@gmail.com>
2023-03-15 18:25:11 +0100

    base page 'base.html' is created. main page 'index.html' is created. styles.css is created. logotip.jpg is added. index view in views.py is adjusted to provide information about books, books' instances and authors to the main page 'index.html'

--- WebBooks/catalog/views.py
+++ WebBooks/catalog/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Author, Book, BookInstance, Genre
+
 
 def index(request):
-    return HttpResponse("Main page of the site")
+    # Amount of books
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    # Available books (in stock)
+    num_instances_available = BookInstance.objects.filter(status__exact=2).count()
+    # Authors
+    num_authors = Author.objects.count()
+
+    return render(request, 'catalog/index.html', context={'num_books': num_books,
+                                                          'num_instances': num_instances,
+                                                          'num_instances_available': num_instances_available,
+                                                          'num_authors': num_authors})
