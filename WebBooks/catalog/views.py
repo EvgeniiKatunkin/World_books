2caf392baa41c3b9bf46bc684a2cfacbe0e587e9
Evgenii <evgeniikatunkin@gmail.com>
2023-03-16 15:55:00 +0100

    views.py: add session counter in index view. index.html: add session counter at the bottom of the page.

--- WebBooks/catalog/views.py
+++ WebBooks/catalog/views.py
@@ -27,7 +27,12 @@ def index(request):
     # Authors
     num_authors = Author.objects.count()
 
+    # Amount of attendances
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     return render(request, 'catalog/index.html', context={'num_books': num_books,
                                                           'num_instances': num_instances,
                                                           'num_instances_available': num_instances_available,
-                                                          'num_authors': num_authors})
+                                                          'num_authors': num_authors,
+                                                          'num_visits': num_visits})
